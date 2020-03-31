45e45b7035da64a423885a0263b59462990ddf86
e125v4 <Kevin.D.Ponseti@ehi.com>
2020-03-31 09:06:11 -0500

    Added swagger and service registration.

--- TestApplication/Startup.cs
+++ TestApplication/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using TestApplication.Static_Extensions;
 
 namespace TestApplication
 {
@@ -15,6 +17,17 @@ namespace TestApplication
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc(setupAction =>
+			{
+				setupAction.ReturnHttpNotAcceptable = true;
+				setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+			});
+
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API V1", Version = "v1" });
+			});
+			ServiceRegistrations.Register(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +37,18 @@ namespace TestApplication
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
-			app.Run(async (context) =>
+			else
 			{
-				await context.Response.WriteAsync("Hello World!");
+				app.UseHsts();
+			}
+			app.UseSwagger();
+
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 			});
+			app.UseHttpsRedirection();
+			app.UseMvc();
 		}
 	}
 }
---
+++ TestApplication/Static Extensions/ServiceRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApplication.Services;
+
+namespace TestApplication.Static_Extensions
+{
+    public static class ServiceRegistrations
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddTransient<IInterestCalcService, InterestCalcService>();
+        }
+    }
+}
